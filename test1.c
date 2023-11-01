a19843f53c236b5318273f24ec41e58d24ef7523
Jovakinn <max05012004@gmail.com>
2023-11-01 10:40:15 +0200

    add tests

---
+++ test1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int a;
+    int x=1123;
+    int y=2232;
+    int z=342414;
+    x=3241;
+    y=105465;
+    z=462645;
+    if(x>5756464) {
+        for(int k=0; k<10; k++) {
+            y = x+3;
+            printf("test!");
+            printf("test!");
+            printf("test!");
+            printf("test!");
+        }
+    } else {
+        int idx = 1;
+    }
+    for(int i=0; i<1022; i++) {
+        printf("test");
+        scanf("%d", &x);
+        if (x>5) {
+            printf("test");
+            printf("test");
+        }
+        for(int j=0; j<z; j++) {
+            a=1;
+            a=1;
+        }
+    }
+    return 1;
+}
---
+++ test2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int x=1;
+    float f;
+    int a=3;
+    int x;
+    float f;
+    a = x * 3 + 5;
+    if(x>a) {
+        if(x>a) {
+            printf("test");
+            a = x - 1;
+        }
+    }
+    else {
+        x = a * 3 + 100;
+    }
+}
